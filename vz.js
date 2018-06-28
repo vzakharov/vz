bb7c776c942d5c48e54b880fc3c250c67f6a2d90
Vova <vzakharov@gmail.com>
2018-06-28 11:55:59 +0300

    deepIterate

---
+++ vz.js
@@ -0,0 +1,29 @@
+const json2csv = require('json2csv')
+const csv2json = require('csvtojson')
+const fs = require('fs')
+
+const _ = require('lodash')
+const {
+    assign, find, map, pullAt, remove
+} = _
+
+const {
+    deepFor, getDiff, sleep
+} = require('./lib/basic')
+
+const {
+    handleFilesRecursively, renameRecursively
+} = require('./lib/files')
+
+const {
+    arrayToHashes, iterate, loadHashes, loadHashesAsArray, readHash, getDeep, setDeep
+} = require('./lib/hash')
+
+const DeepIterate = require('./lib/deepIterate')
+
+module.exports = {
+    deepFor, getDiff, sleep,
+    handleFilesRecursively, renameRecursively,
+    arrayToHashes, iterate, loadHashes, loadHashesAsArray, readHash, getDeep, setDeep,
+    DeepIterate
+}
